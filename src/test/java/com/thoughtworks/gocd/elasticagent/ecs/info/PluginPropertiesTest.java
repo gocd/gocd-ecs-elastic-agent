a637b50fccec54cbce10101f3c819831d0adf7f8
Kritika Singh <kritika.singh1994@gmail.com>
2020-11-12 16:19:44 +0530

    Updating to github actions
     - testing the PR build
     - testing and releasing an exp version of the jar via Java 15
     - making a stable release
    
    Also updated the plugin version nd the min GoCD version to 20.9.0

--- src/test/java/com/thoughtworks/gocd/elasticagent/ecs/info/PluginPropertiesTest.java
+++ src/test/java/com/thoughtworks/gocd/elasticagent/ecs/info/PluginPropertiesTest.java
@@ -30,7 +30,7 @@ class PluginPropertiesTest {
         assertThat(properties.get("description")).isEqualTo("GoCD Elastic Agent Plugin for Amazon Elastic Container Service allow for more efficient use of instances");
         assertThat(properties.get("vendorName")).isEqualTo("ThoughtWorks, Inc.");
         assertThat(properties.get("vendorUrl")).isEqualTo("https://github.com/gocd/gocd-ecs-elastic-agent");
-        assertThat(properties.get("goCdVersion")).isEqualTo("19.3.0");
+        assertThat(properties.get("goCdVersion")).isEqualTo("20.9.0");
         assertThat(properties.get("version")).isNotBlank();
     }
 }
